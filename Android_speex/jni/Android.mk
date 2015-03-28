51b7ad5ff044bee9c082de2de5267995c99568e4
Hunter_Li <lgjx123@163.com>
2015-03-28 12:05:07 +0800

    use cygwin compile env

--- Android_speex/jni/Android.mk
+++ Android_speex/jni/Android.mk
@@ -1,27 +1,63 @@
-LOCAL_PATH := $(call my-dir)
+﻿LOCAL_PATH:= $(call my-dir)/
 
+#编译参数设置
+MYCOMPILE_CFLAG:= -g -D_ANDROID_ -DFIXED_POINT -DUSE_KISS_FFT -DEXPORT="" -UHAVE_CONFIG_H
+MYCOMPILE_CXXFLAG:= -g -D_ANDROID_  -fpermissive
+
+################## 头文件设置 ##################
+
+#头文件目录
+INC_DIR:=../../speex-1.2rc1/include
+
+# 需要排除的目录,要以'%'打头,中间用空格分隔
+EXCLUDE_DIRS := %IOS %Win 
+
+# 需要排除的文件,要以'%'打头,中间用空格分隔
+EXCLUDE_FILES := 
+
+# 取得当前目录的所有子目录名称
+INC_DIRTREE := $(shell find $(LOCAL_PATH)$(INC_DIR) -maxdepth 99 -type d)
+INC_DIRTREE := $(filter-out $(EXCLUDE_DIRS),$(INC_DIRTREE))
+
+################## 源码文件设置 ##################
+
+#源码根目录
+SRC_DIR:=../../speex-1.2rc1/libspeex
+
+$(warning $(SRC_DIR))
+$(warning $(INC_DIR))
+
+# 需要排除的目录,要以'%'打头,中间用空格分隔
+EXCLUDE_DIRS := %IOS %Win32 %Win32/ImpSound 
+
+# 需要排除的文件,要以'%'打头,中间用空格分隔
+EXCLUDE_FILES := 
+
+# 取得当前目录的所有子目录名称
+SRC_DIR := $(shell find $(LOCAL_PATH)$(SRC_DIR) -maxdepth 99 -type d)
+SRC_DIR := $(filter-out $(EXCLUDE_DIRS),$(SRC_DIR))
+
+SRC_FILES       := $(foreach sdir,$(SRC_DIR),$(wildcard $(sdir)/*.cpp))
+SRC_FILES       += $(foreach sdir,$(SRC_DIR),$(wildcard $(sdir)/*.c))
+SRC_FILES		:= $(subst $(LOCAL_PATH),,$(SRC_FILES))
+
+#过滤掉某些不能编译进去的cpp
+SRC_FILES := $(filter-out $(EXCLUDE_FILES), $(SRC_FILES))
+
+#生成 Spexx 库
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := speex
+LOCAL_C_INCLUDES:=$(INC_DIRTREE)
+LOCAL_C_INCLUDES+=$(SRC_DIR)
 
-LOCAL_CFLAGS = -DFIXED_POINT -DUSE_KISS_FFT -DEXPORT="" -UHAVE_CONFIG_H
+LOCAL_SRC_FILES = $(SRC_FILES)
 
-INC_DIR:=..\speex-1.2rc1\include  
-SRC_PATH:=..\speex-1.2rc1\libspeex
+#生成库名
+LOCAL_MODULE:=speex
 
-curdir:=$(shell cd)
-SRCDIRTREE := $(curdir)\$(SRC_PATH)
+LOCAL_STATIC_LIBRARIES:=
 
-#���ɱ����Դ�ļ�
-SRC_FILES := $(foreach sdir,$(SRCDIRTREE),$(wildcard $(sdir)/*.cpp))
-SRC_FILES += $(foreach sdir,$(SRCDIRTREE),$(wildcard $(sdir)/*.c))
-SRC_FILES := $(subst $(LOCAL_PATH),,$(SRC_FILES))
+LOCAL_CFLAGS := $(MYCOMPILE_CFLAG)
+LOCAL_CXXFLAGS:= $(MYCOMPILE_CXXFLAG)
 
-LOCAL_SRC_FILES := $(SRC_FILES)
-LOCAL_C_INCLUDES:=$(curdir)\$(INC_DIR)
-
-#$(warning $(LOCAL_SRC_FILES))
-#$(warning $(LOCAL_C_INCLUDES))
-
-#include $(BUILD_SHARED_LIBRARY)
 include $(BUILD_STATIC_LIBRARY)
--- Android_speex/jni/Application.mk
+++ Android_speex/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_PROJECT_PATH:=$(call my-dir)/..
+﻿APP_PROJECT_PATH:=$(call my-dir)/..
 APP_MODULES := speex
 APP_ABI:=armeabi
 APP_STL:=stlport_static
